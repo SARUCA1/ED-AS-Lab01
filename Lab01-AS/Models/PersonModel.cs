85cfd1b1060a65a1d222cfb9c180180f1b7a62cc
Alejandro Gil <alejandro115935@gmail.com>
2022-02-07 21:34:19 -0600

    Modelos 2

--- Lab01-AS/Models/PersonModel.cs
+++ Lab01-AS/Models/PersonModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace Lab01_AS.Models
 {
     public class PersonModel
     {
+        public int ID { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        [MaxLength(11)]
+        public int PhoneNumber { get; set; }
+        [MaxLength(50)]
+        public string Description { get; set; }
     }
 }
